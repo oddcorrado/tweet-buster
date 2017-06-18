afd2a0922ed3d5c24c80b9db93a63df7152eb6cf
corrado_pb <corrado@pixelbizarre.com>
2017-06-18 19:30:21 +0200

    twitter client setup and display twitter on debug (for now)

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ tweet-buster
 `tweet-buster` is an exercise I am currently building for an internship contest
 
 This a work-in-progress... stay tuned
+
+Install
+-------
+
+npm install
+
+Usage
+-----
+
+You will need valid Twitter developer credentials in the form of a set of consumer and access tokens/keys.  You can get these [here](https://apps.twitter.com/).  For this app you only need to set-up read permissions.
+
+You can provide your credentials either through environment variables in: TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN_KEY and TWITTER_ACCESS_TOKEN_SECRET
+
+Or in the secret config folder : simply create a twitter.js file in config/secret with the following content and set your keys/tokens accordingly:
+module.exports = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const debug = require('debug')('app')
 const app = express()
+const twitterClient = require('./lib/twitterClient')
+const _ = require('lodash')
 
 module.exports = app
 
@@ -19,4 +21,22 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
   debug('Page not found...')
   res.sendStatus(404)
+})
+
+// create the hashstream -- this will be moved just for quick test purpose
+debug('Starting hashstream')
+let hashStream = twitterClient.stream('statuses/filter', {track: '#trump'})
+hashStream.on('data', function(event) {
+  // TODO just to make sure this is really a tweet, might need more insight later
+  if(event && _.conformsTo(event, {id_str: _.isString, text: _.isString}))
+  {
+    debug("*************")
+    debug(event.text)
+  }
+})
+
+hashStream.on('error', function(error) {
+  debug('could not start twitter client')
+  debug('Error:', error)
+  throw error;
 })
