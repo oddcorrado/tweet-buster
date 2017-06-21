2aee1df3c6f80332f9c8cd7c49233a324ee1412b
corrado <corradolongoni@yahoo.com>
2017-06-21 02:50:36 +0200

    added connection management to start/stop the tweeter stream depending on socket io client reference count

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const twitterClient = require('./lib/twitterClient')
 const socketioBroadcaster = require('./lib/socketioBroadcaster')
 const _ = require('lodash')
 const config = require('./config')
+const events = require('events')
 
 module.exports = app
 
@@ -23,21 +24,39 @@ app.use((req, res) => {
   res.sendStatus(404)
 })
 
-// create the hashstream -- this will be moved just for quick test purpose
-debug('Starting hashstream')
-let hashStream = twitterClient.stream('statuses/filter', {track: config.hashtag})
-hashStream.on('data', function (event) {
-  // TODO just to make sure this is really a tweet, might need more insight later
-  if(event && _.conformsTo(event, {id_str: _.isString, text: _.isString}))
-  {
-    debugTweets(event.text)
-    // simply tell socketioCaster to broadcast the message
-    socketioBroadcaster.broadcast(event.text)
-  }
+// Creating an event Emitter for the broadcaster so that we only stream if necessary
+// the broadcaster will emit an  activate event when it is active
+// and a deactivate event when it is inactive
+let socketioBroadcasterEmitter = new events.EventEmitter()
+let hashStream = null
+
+// Register this emitter to the broacast emitter
+socketioBroadcaster.registerEmitter(socketioBroadcasterEmitter)
+
+// When the broacaster is active start the tweeter stream
+socketioBroadcasterEmitter.on('activate',() => {
+  // create the hashstream
+  debug('Starting hashstream')
+  hashStream = twitterClient.stream('statuses/filter', {track: config.hashtag})
+  hashStream.on('data', function (event) {
+    // TODO just to make sure this is really a tweet, might need more insight later
+    if(event && _.conformsTo(event, {id_str: _.isString, text: _.isString}))
+    {
+      debugTweets(event.text)
+      // simply tell socketioCaster to broadcast the message
+      socketioBroadcaster.broadcast(event.text)
+    }
+  })
+
+  hashStream.on('error', function (error) {
+    debug('could not start twitter client')
+    debug('Error:', error)
+    throw error
+  })
 })
 
-hashStream.on('error', function (error) {
-  debug('could not start twitter client')
-  debug('Error:', error)
-  throw error
+// When the broacaster is inactive destroy the tweeter stream
+socketioBroadcasterEmitter.on('deactivate',() => {
+  debug('Destroying hashstream')
+  hashStream.destroy()
 })
