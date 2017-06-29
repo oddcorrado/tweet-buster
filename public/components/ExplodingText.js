4c4f7ae5fa591dbefd3f6f937e34bae4c902aab7
corrado <corradolongoni@yahoo.com>
2017-06-30 00:01:44 +0200

    explode tweets into words instead of letters, improves clarity and reduces lag

--- public/components/ExplodingText.js
+++ public/components/ExplodingText.js
@@ -1,89 +1,76 @@
 const PIXI = require('pixi.js')
 
 // This is an exploding text
-// it takes a text as input and explodes it in rotating letters
+// it takes a text as input and explodes it in rotating words
 class ExplodingText {
   constructor (text, stage, style, position, options) {
     // grab the options
     options = options || {}
     options.duration = options.duration || 1000
-    options.precise = options.precise !== undefined ? options.precise : false
     options.velocity = options.velocity || 10
 
     // private members a la Crockford
     // ***********
-    // letter and metrics for font explosion
-    let letters = []
+    // Word and metrics for font explosion
+    let words = []
     // time to live of the effect in ms
     let ttl = options.duration
 
     // private methods a la Crockford declared in the constructor
     // ***********
-    // measures precise text metrics for text explosion
-    let measureMetricsPrecise = () => {
-      let pos = text.length - 1
-
-      while (pos >= 0) {
-        let metrics = PIXI.TextMetrics.measureText(text.substr(0,pos), style)
-        if(text[pos].match(/[^\r\n\s\t]/)) {
-          letters.push({char:text[pos],
-            x:metrics.width,
-            y:metrics.height,
-            textPos:pos / text.length
-          })
-        }
-        pos--
-      }
-    }
-
     // measures coarse text metrics for text explosion -- less precise but avoids lag when exploding
     // only works for one line text
     let measureMetricsCoarse = () => {
-      let pos = text.length - 1
-
+      let pos = 0
       let metrics = PIXI.TextMetrics.measureText(text, style)
-      while (pos >= 0) {
-        if(text[pos].match(/[^\r\n\s\t]/)) {
-          letters.push({char:text[pos],
-            x:metrics.width * pos/text.length,
-            y:metrics.height,
-            textPos:pos/text.length
-          })
-        }
-        pos--
-      }
+
+      // split text into words according to space
+      // then map to object containing metrics
+      words = text.match(/([^\s]+)/g)
+        .map(word => {
+          let out = { word, // resumting word
+            x:metrics.width * pos/text.length, // x offset in pixels
+            y:metrics.height, // y offset in pixels
+            textPos:pos/text.length // relative position in text (used for explosion direction)
+          }
+          pos += word.length + 1
+          return out
+        })
     }
 
-    // creates text metrics for text explosion
-    let createLetters = () => {
-      letters = letters.map(letter => {
-        let pixiLetter = new PIXI.Text(letter.char, style)
-        stage.addChild(pixiLetter)
-        pixiLetter.x = position.x + letter.x
-        pixiLetter.y = position.y + letter.y
-        pixiLetter.anchor.x = 0.5
-        pixiLetter.anchor.y = 0.5
-        return { pixiLetter,
-          vx : options.velocity * (letter.textPos - 0.5),
+    // creates PIXI text words for explosion
+    let createWords = () => {
+      // use the words array and remap it to PIXI Texts
+      // add the position offset to each word
+      // and set random velocities for explosion
+      words = words.map(element => {
+        let pixiWord = new PIXI.Text(element.word, style)
+        stage.addChild(pixiWord)
+        pixiWord.x = position.x + element.x
+        pixiWord.y = position.y + element.y
+        pixiWord.anchor.x = 0.5
+        pixiWord.anchor.y = 0.5
+        return { pixiWord,
+          vx : options.velocity * (element.textPos - 0.5),
           vy : options.velocity * (Math.random() - 0.5),
-          vrot : Math.random() - 0.5}
+          vrot : 0.2*(Math.random() - 0.5)}
       })
     }
 
     // move is called ot regulazr intervals every 16ms/60fps
     let move = () => {
-      // get each letter and move it according to is speed
-      letters.forEach(letter => {
-        letter.pixiLetter.x += letter.vx
-        letter.pixiLetter.y += letter.vy
-        letter.pixiLetter.rotation += letter.vrot
-        letter.pixiLetter.alpha = ttl / options.duration
+      // get each Word and move it according to is speed
+      words.forEach(word => {
+        word.pixiWord.x += word.vx
+        word.pixiWord.y += word.vy
+        word.pixiWord.rotation += word.vrot
+        word.pixiWord.alpha = ttl / options.duration
       })
 
       // remove when dead
       if (ttl < 0 ) {
-        letters.forEach(letter => stage.removeChild(letter.pixiLetter))
-        letters = null
+        words.forEach(word => stage.removeChild(word.pixiWord))
+        words = null
       }
       else {
         setTimeout(() => move(), 16)
@@ -94,15 +81,11 @@ class ExplodingText {
     // intialisation
     // *************
     // measure text metrics
-    if(options.precise) {
-      measureMetricsPrecise()
-    } else {
-      measureMetricsCoarse()
-    }
+    measureMetricsCoarse()
 
-    // create letters and add them to the stage
+    // create words and add them to the stage
     // TODO check if we can optimize with ParticleContainer
-    createLetters()
+    createWords()
 
     // start to move
     move()
